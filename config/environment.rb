92863f602db5909a3bbe7230f0539067cfb3140c
Will Strinz <wstrinz@gmail.com>
2013-02-28 10:23:01 -0600

    Revert "working on heroku-redis connect"
    This reverts commit 013e887e111ecaefd5ebe093f5e0b480940eea4e.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-ENV["REDISTOGO_URL"] = 'redis://redistogo:1f736fa2a27319dc45b7ebb470e04bbe@dory.redistogo.com:10177/'
-
 # Initialize the rails application
 GrowingPower::Application.initialize!
