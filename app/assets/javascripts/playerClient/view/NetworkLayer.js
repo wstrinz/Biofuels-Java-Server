6e25bc8a1be935fdf9fffa800041542d6f8183f8
Will Strinz <wstrinz@gmail.com>
2013-03-21 11:03:13 -0500

    hide management icons

--- app/assets/javascripts/playerClient/view/NetworkLayer.js
+++ app/assets/javascripts/playerClient/view/NetworkLayer.js
@@ -28,8 +28,8 @@ Ext.define('Biofuels.view.NetworkLayer', {
 
     // var WS = window['MozWebSocket'] ? MozWebSocket : WebSocket;
 
-    WsConnection.webSocket = new WebSocketRails('http://sleepy-temple-8942.herokuapp.com:80/websocket', false);
-    // WsConnection.webSocket = new WebSocketRails('localhost:3000/websocket');
+    // WsConnection.webSocket = new WebSocketRails('http://sleepy-temple-8942.herokuapp.com:80/websocket', false);
+    WsConnection.webSocket = new WebSocketRails('localhost:3000/websocket');
 
     var self = this;
     WsConnection.webSocket.on_open = function() {
